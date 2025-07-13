b5f8e88d92d086be3863031273d34c6284dad752
abhinava31 <abhinava@hornet.technology>
2025-07-13 22:16:49 +0530

    test

--- pkg/case/cases.go
+++ pkg/case/cases.go
@@ -9,4 +9,7 @@ type Repository interface {
 	GetAllCases() ([]factory.Report, error)
 	// GetCasebyUser retrieves cases by user email.
 	GetCasebyUser(email string) ([]factory.Report, error)
+
+	// GetCaseByID retrieves a case by its ID.
+	GetCaseByID(id string) (factory.Report, error)
 }
